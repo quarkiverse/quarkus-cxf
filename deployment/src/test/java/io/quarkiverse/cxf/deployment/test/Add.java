de2a8965f4a400343d0e626639fc9cc6198318d8
olivier dufour <olivier.duff@gmail.com>
2020-11-01 12:23:24 +0100

    add support for Requestwrapper and ResponseWrapper support

---
+++ deployment/src/test/java/io/quarkiverse/cxf/deployment/test/Add.java
@@ -0,0 +1,35 @@
+package io.quarkiverse.cxf.deployment.test;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "fruit"
+})
+@XmlRootElement(name = "Add")
+public class Add {
+
+    @XmlElement(name = "fruit")
+    protected Fruit fruit;
+
+    /**
+     * Obtient la valeur de la propriété addResult.
+     *
+     */
+    public Fruit getFruit() {
+        return fruit;
+    }
+
+    /**
+     * Définit la valeur de la propriété addResult.
+     *
+     */
+    public void setFruit(Fruit value) {
+        this.fruit = value;
+    }
+
+}
