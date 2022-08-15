702f03116e9d1df565d22c6891e4f482ec1d681a
Peter Palaga <ppalaga@redhat.com>
2022-08-15 15:56:50 +0200

    Test the SOAP handler in native mode

---
+++ integration-tests/basic/src/test/java/io/quarkiverse/it/cxf/GreetingSOAPHandlerIT.java
@@ -0,0 +1,7 @@
+package io.quarkiverse.it.cxf;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingSOAPHandlerIT extends GreetingSOAPHandlerTest {
+}
