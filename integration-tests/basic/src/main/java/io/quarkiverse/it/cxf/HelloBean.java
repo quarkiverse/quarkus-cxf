9527e0772a758359afd35f59de8ab66512a5cee5
Peter Palaga <ppalaga@redhat.com>
2022-08-15 14:02:32 +0200

    Rename HelloResource to HelloBean to avoid the impression that it is a REST resource or anything else

--- integration-tests/basic/src/main/java/io/quarkiverse/it/cxf/HelloBean.java
+++ integration-tests/basic/src/main/java/io/quarkiverse/it/cxf/HelloBean.java
@@ -2,8 +2,11 @@ package io.quarkiverse.it.cxf;
 
 import javax.inject.Singleton;
 
+/**
+ * A simple bean for testing whether injecting into service implementation works.
+ */
 @Singleton
-public class HelloResource {
+public class HelloBean {
     public String getHello() {
         return "Hello ";
     }
