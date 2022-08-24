a39f28924f495cf4153c4971208ef44defe7f316
Peter Palaga <ppalaga@redhat.com>
2022-08-24 12:08:09 +0200

    Remove reflection registrations from QuarkusCxfLoggingProcessor done in QuarkusCxfProcessor.reflectiveClasses()

--- extensions/features-logging/deployment/src/main/java/io/quarkiverse/cxf/deployment/QuarkusCxfLoggingProcessor.java
+++ extensions/features-logging/deployment/src/main/java/io/quarkiverse/cxf/deployment/QuarkusCxfLoggingProcessor.java
@@ -2,34 +2,16 @@ package io.quarkiverse.cxf.deployment;
 
 import java.util.stream.Stream;
 
-import org.apache.cxf.ext.logging.LoggingFeature;
-import org.apache.cxf.ext.logging.LoggingInInterceptor;
-import org.apache.cxf.ext.logging.LoggingOutInterceptor;
-import org.apache.cxf.feature.AbstractPortableFeature;
-import org.apache.cxf.feature.DelegatingFeature;
-
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 
 public class QuarkusCxfLoggingProcessor {
 
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem("cxf-rt-features-logging");
-    }
-
-    @BuildStep
-    void registerLoggingReflectionItems(BuildProducer<ReflectiveClassBuildItem> reflectiveItems) {
-        reflectiveItems.produce(new ReflectiveClassBuildItem(true, false,
-                LoggingFeature.class.getName(),
-                AbstractPortableFeature.class.getName(),
-                DelegatingFeature.class.getName(),
-                LoggingFeature.Portable.class.getName(),
-                LoggingInInterceptor.class.getName(),
-                LoggingOutInterceptor.class.getName()));
     }
 
     @BuildStep
