acb99c60bd8617e80d80886ec0d24d1cd4f0041a
Peter Palaga <ppalaga@redhat.com>
2022-10-28 17:40:12 +0200

    Test MTOM with an AWT Image without an explicit Response/RequestWrapper

--- integration-tests/mtom-awt/src/main/java/io/quarkiverse/cxf/it/ws/mtom/awt/server/ImageService.java
+++ integration-tests/mtom-awt/src/main/java/io/quarkiverse/cxf/it/ws/mtom/awt/server/ImageService.java
@@ -1,19 +1,25 @@
 package io.quarkiverse.cxf.it.ws.mtom.awt.server;
 
+import java.awt.Image;
+
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
 import javax.xml.ws.soap.MTOM;
 
-@WebService(name = "ImageService", targetNamespace = "https://quarkiverse.github.io/quarkiverse-docs/quarkus-cxf/test/mtom-awt")
-@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, parameterStyle = SOAPBinding.ParameterStyle.BARE)
+@WebService(name = "ImageService", targetNamespace = ImageService.NS)
 @MTOM
 public interface ImageService {
 
-    @WebMethod
-    ImageData downloadImage(String name);
+    public static final String NS = "https://quarkiverse.github.io/quarkiverse-docs/quarkus-cxf/test/mtom-awt";
 
     @WebMethod
-    String uploadImage(ImageData image);
+    Image downloadImage(
+            @WebParam(name = "name", targetNamespace = NS) String name);
+
+    @WebMethod
+    String uploadImage(
+            @WebParam(name = "data", targetNamespace = NS) Image data,
+            @WebParam(name = "name", targetNamespace = NS) String name);
 
 }
