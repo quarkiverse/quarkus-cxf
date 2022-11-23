f8a5995f65006463a991d1fc2786db39d30f5839
Peter Palaga <ppalaga@redhat.com>
2022-11-23 20:10:12 +0100

    ClassNotFoundException: MessageContext when quarkus-cxf-services-sts is combined with quarkus-resteasy #607

--- extensions/services-sts/deployment/src/main/java/io/quarkiverse/cxf/services/sts/deployment/QuarkusCxfStsProcessor.java
+++ extensions/services-sts/deployment/src/main/java/io/quarkiverse/cxf/services/sts/deployment/QuarkusCxfStsProcessor.java
@@ -2,8 +2,15 @@ package io.quarkiverse.cxf.services.sts.deployment;
 
 import java.util.stream.Stream;
 
+import org.apache.cxf.sts.rest.RESTSecurityTokenService;
+import org.apache.cxf.sts.rest.RESTSecurityTokenServiceImpl;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.IndexView;
+
+import io.quarkus.arc.deployment.BuildTimeConditionBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.IndexDependencyBuildItem;
 
@@ -24,6 +31,24 @@ public class QuarkusCxfStsProcessor {
                     String[] coords = ga.split(":");
                     indexDependencies.produce(new IndexDependencyBuildItem(coords[0], coords[1]));
                 });
+    }
+
+    @BuildStep
+    void buildTimeConditions(CombinedIndexBuildItem combinedIndexBuildItem,
+            BuildProducer<BuildTimeConditionBuildItem> buildTimeConditions) {
+
+        final IndexView index = combinedIndexBuildItem.getIndex();
+
+        /*
+         * RESTSecurityTokenService and RESTSecurityTokenServiceImpl have JAX-RS annotations
+         * and thus quarkus-resteasy (if available in the class path) wants to deploy them
+         * But they would not work because they are missing some Spring stuff that we exclude
+         * So we rather hide them from RESTeasy
+         */
+        Stream.of(RESTSecurityTokenService.class, RESTSecurityTokenServiceImpl.class)
+                .map(cl -> new BuildTimeConditionBuildItem(index.getClassByName(DotName.createSimple(cl.getName())), false))
+                .forEach(buildTimeConditions::produce);
+
     }
 
 }
