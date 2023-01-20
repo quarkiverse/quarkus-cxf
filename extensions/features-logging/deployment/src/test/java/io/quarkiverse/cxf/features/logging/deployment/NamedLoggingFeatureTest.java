ed156392ecc94547009e14c4206a6ed47d72af4f
Peter Palaga <ppalaga@redhat.com>
2023-01-20 22:04:48 +0100

    Get rid of @Unremovable if quarkus-cxf-integration-test-logging is in the class path

---
+++ extensions/features-logging/deployment/src/test/java/io/quarkiverse/cxf/features/logging/deployment/NamedLoggingFeatureTest.java
@@ -0,0 +1,59 @@
+package io.quarkiverse.cxf.features.logging.deployment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.util.logging.LogRecord;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkiverse.cxf.annotation.CXFClient;
+import io.quarkus.test.QuarkusUnitTest;
+
+/**
+ * {@code LoggingFeature} set in {@code application.properties} and a named bean is produced via
+ * {@link NamedLoggingFeatureProducer}
+ */
+public class NamedLoggingFeatureTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest TEST = new QuarkusUnitTest()
+            .withApplicationRoot(
+                    root -> root.addClasses(HelloService.class, HelloServiceImpl.class, NamedLoggingFeatureProducer.class))
+            .overrideConfigKey("quarkus.cxf.endpoint.\"/hello\".implementor", HelloServiceImpl.class.getName())
+            .overrideConfigKey("quarkus.cxf.endpoint.\"/hello\".features", "#namedLoggingFeature")
+            .overrideConfigKey("quarkus.cxf.client.hello.service-interface", HelloService.class.getName())
+            .overrideConfigKey("quarkus.cxf.client.hello.client-endpoint-url", "http://localhost:8081/hello")
+            .setLogRecordPredicate(logRecord -> logRecord.getLoggerName().contains("org.apache.cxf.services.HelloService.RE")) // REQ_IN or RESP_OUT
+            .assertLogRecords(records -> assertThat(records)
+                    .extracting(LogRecord::getMessage)
+                    .anyMatch(msg -> msg.contains(
+                            "Payload: <soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                                    + "  <soap:Body>\n"
+                                    + "    <ns1:hello xmlns:ns1=\"http://deployment.logging.features.cxf.quarkiverse.io/\">\n"
+                                    + "      <arg0>Dolly</arg0>\n"
+                                    + "    </ns1:hello>\n"
+                                    + "  </soap:Body>\n"
+                                    + "</soap:Envelope>"))
+                    .anyMatch(msg -> msg.contains(
+                            "Payload: <soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                                    + "  <soap:Body>\n"
+                                    + "    <ns1:helloResponse xmlns:ns1=\"http://deployment.logging.features.cxf.quarkiverse.io/\">\n"
+                                    + "      <return>Hello Dolly!</return>\n"
+                                    + "    </ns1:helloResponse>\n"
+                                    + "  </soap:Body>\n"
+                                    + "</soap:Envelope>"))
+                    .hasSize(2));
+
+    @CXFClient("hello")
+    HelloService helloService;
+
+    @Test
+    void payloadPrettyLogged() throws IOException {
+
+        helloService.hello("Dolly");
+
+    }
+
+}
