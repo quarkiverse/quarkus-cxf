afe1c3eda7eed7d9c63e2be10487f9a6ab417daa
Peter Palaga <ppalaga@redhat.com>
2023-03-24 08:55:34 +0100

    Test MTOM java.awt.Image service with and without explicit wrappers

--- integration-tests/mtom-awt/src/main/java/io/quarkiverse/cxf/it/ws/mtom/awt/server/MtomAwtResource.java
+++ integration-tests/mtom-awt/src/main/java/io/quarkiverse/cxf/it/ws/mtom/awt/server/MtomAwtResource.java
@@ -23,29 +23,60 @@ import io.quarkiverse.cxf.annotation.CXFClient;
 @ApplicationScoped
 public class MtomAwtResource {
 
+    public enum ClientKey {
+        imageServiceClient,
+        imageServiceClientWithWrappers
+    }
+
     @Inject
     @CXFClient("imageServiceClient")
     ImageService imageServiceClient;
 
-    @Path("/image/{imageName}")
+    @Inject
+    @CXFClient("imageServiceClientWithWrappers")
+    ImageServiceWithWrappers imageServiceClientWithWrappers;
+
+    @Path("/image/{clientKey}/{imageName}")
     @POST
     @Consumes(MediaType.APPLICATION_OCTET_STREAM)
     @Produces(MediaType.TEXT_PLAIN)
-    public Response upload(@PathParam("imageName") String imageName, InputStream in) throws Exception {
-        String response = imageServiceClient.uploadImage(new ImageData(ImageIO.read(in), imageName));
+    public Response upload(@PathParam("clientKey") ClientKey clientKey, @PathParam("imageName") String imageName,
+            InputStream in) throws Exception {
+        String response = null;
+        switch (clientKey) {
+            case imageServiceClient:
+                response = imageServiceClient.uploadImage(ImageIO.read(in), imageName);
+                break;
+            case imageServiceClientWithWrappers:
+                response = imageServiceClientWithWrappers.uploadImage(ImageIO.read(in), imageName);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected " + ClientKey.class.getName() + ": " + clientKey.name());
+        }
         return Response
                 .created(new URI("https://quarkus.io/"))
                 .entity(response)
                 .build();
     }
 
-    @Path("/image/{imageName}")
+    @Path("/image/{clientKey}/{imageName}")
     @GET
     @Consumes(MediaType.TEXT_PLAIN)
-    public byte[] download(@PathParam("imageName") String imageName) throws Exception {
-        ImageData image = imageServiceClient.downloadImage(imageName);
+    public byte[] download(@PathParam("clientKey") ClientKey clientKey, @PathParam("imageName") String imageName)
+            throws Exception {
+        java.awt.Image image = null;
+        switch (clientKey) {
+            case imageServiceClient:
+                image = imageServiceClient.downloadImage(imageName);
+                break;
+            case imageServiceClientWithWrappers:
+                image = imageServiceClientWithWrappers.downloadImage(imageName);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected " + ClientKey.class.getName() + ": " + clientKey.name());
+        }
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-            ImageIO.write((BufferedImage) image.getData(), "png", baos);
+            ImageIO.write((BufferedImage) image, "png", baos);
             return baos.toByteArray();
         }
     }
