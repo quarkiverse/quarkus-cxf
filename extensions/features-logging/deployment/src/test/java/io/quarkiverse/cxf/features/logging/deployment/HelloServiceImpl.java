48d22f059b211fe10ef99467e3cae5edf8311bc3
Peter Palaga <ppalaga@redhat.com>
2023-04-11 09:54:18 +0200

    Test endpoints having no configuration in application.properties #552

--- extensions/features-logging/deployment/src/test/java/io/quarkiverse/cxf/features/logging/deployment/HelloServiceImpl.java
+++ extensions/features-logging/deployment/src/test/java/io/quarkiverse/cxf/features/logging/deployment/HelloServiceImpl.java
@@ -3,10 +3,14 @@ package io.quarkiverse.cxf.features.logging.deployment;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import org.apache.cxf.ext.logging.LoggingFeature;
+import org.apache.cxf.feature.Features;
+
 /**
  * The simplest Hello service implementation.
  */
 @WebService(serviceName = "HelloService")
+@Features(classes = { LoggingFeature.class })
 public class HelloServiceImpl implements HelloService {
 
     @WebMethod
