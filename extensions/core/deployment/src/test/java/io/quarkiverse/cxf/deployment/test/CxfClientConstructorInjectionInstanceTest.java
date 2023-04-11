db5c382b2a537746243155a6dd3a369718622c75
Peter Palaga <ppalaga@redhat.com>
2023-04-11 22:35:27 +0200

    Use inline configuartion instead of external properties in deployment tests

--- extensions/core/deployment/src/test/java/io/quarkiverse/cxf/deployment/test/CxfClientConstructorInjectionInstanceTest.java
+++ extensions/core/deployment/src/test/java/io/quarkiverse/cxf/deployment/test/CxfClientConstructorInjectionInstanceTest.java
@@ -24,7 +24,11 @@ public class CxfClientConstructorInjectionInstanceTest {
                     .addClass(Add.class)
                     .addClass(Delete.class)
                     .addClass(CxfClientConstructorInjectionInstanceBean.class))
-            .withConfigurationResource("application-cxf-test.properties");
+            .overrideConfigKey("quarkus.cxf.client.\"fruitclient\".client-endpoint-url", "http://localhost:8081/fruit")
+            .overrideConfigKey("quarkus.cxf.client.\"fruitclient\".service-interface",
+                    "io.quarkiverse.cxf.deployment.test.FruitWebService")
+            .overrideConfigKey("quarkus.cxf.client.\"foo\".client-endpoint-url", "http://localhost:8081/fruit")
+            .overrideConfigKey("quarkus.cxf.client.\"foo\".features", "org.apache.cxf.feature.LoggingFeature");
 
     @Inject
     CxfClientConstructorInjectionInstanceBean bean;
--- extensions/core/deployment/src/test/java/io/quarkiverse/cxf/deployment/test/CxfSeiOnlyClientTest.java
+++ extensions/core/deployment/src/test/java/io/quarkiverse/cxf/deployment/test/CxfSeiOnlyClientTest.java
@@ -26,7 +26,11 @@ public class CxfSeiOnlyClientTest {
                     .addClass(Add.class)
                     .addClass(Delete.class)
                     .addClass(GreetingWebService.class))
-            .withConfigurationResource("application-cxf-test.properties");
+            .overrideConfigKey("quarkus.cxf.client.\"fruitclient\".client-endpoint-url", "http://localhost:8081/fruit")
+            .overrideConfigKey("quarkus.cxf.client.\"fruitclient\".service-interface",
+                    "io.quarkiverse.cxf.deployment.test.FruitWebService")
+            .overrideConfigKey("quarkus.cxf.client.\"foo\".client-endpoint-url", "http://localhost:8081/fruit")
+            .overrideConfigKey("quarkus.cxf.client.\"foo\".features", "org.apache.cxf.feature.LoggingFeature");
 
     @Inject
     CXFClientInfo clientInfo;
