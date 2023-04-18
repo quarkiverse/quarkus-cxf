0cb14ed7e47f110cbc306cdcb520a42b319a40a8
Peter Palaga <ppalaga@redhat.com>
2023-04-18 16:39:28 +0200

    WebFaults missing in the WSDL served in native mode #821

--- integration-tests/server/src/main/java/io/quarkiverse/cxf/it/server/FaultyHelloServiceImpl.java
+++ integration-tests/server/src/main/java/io/quarkiverse/cxf/it/server/FaultyHelloServiceImpl.java
@@ -2,18 +2,16 @@ package io.quarkiverse.cxf.it.server;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
-import javax.xml.ws.BindingType;
 
 /**
  * A Web service always throwing an exception.
  */
-@WebService(endpointInterface = "io.quarkiverse.cxf.it.server.FaultyHelloService", serviceName = "FaultyHelloService")
-@BindingType(javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
+@WebService(serviceName = "FaultyHelloService")
 public class FaultyHelloServiceImpl implements FaultyHelloService {
 
     @WebMethod
     @Override
-    public String hello(String text) throws GreetingException {
+    public String faultyHello(String text) throws GreetingException {
         throw new GreetingException("foo", "bar");
     }
 
