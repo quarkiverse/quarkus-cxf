41e8beaa2f2ebe40e61e45413ff2608761c33d69
Peter Palaga <ppalaga@redhat.com>
2023-08-31 23:37:06 +0200

    Introduce quarkus.cxf[.client.myClient].http-conduit-factory as a configurable workaround for #992 CXF clients based on java.net.http.HttpClient leak threads

--- integration-tests/mtom/src/test/java/io/quarkiverse/cxf/it/ws/mtom/server/MtomIT.java
+++ integration-tests/mtom/src/test/java/io/quarkiverse/cxf/it/ws/mtom/server/MtomIT.java
@@ -1,8 +1,19 @@
 package io.quarkiverse.cxf.it.ws.mtom.server;
 
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.transport.http.HTTPConduitFactory;
+
+import io.quarkiverse.cxf.URLConnectionHTTPConduitFactory;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
 public class MtomIT extends MtomTest {
 
+    public MtomIT() {
+        /*
+         * quarkus.cxf.http-conduit-factory = URLConnectionHTTPConduitFactory is not effective for the JVM
+         * running the native tests. Thus we have to set the ConduitFactory manually.
+         */
+        BusFactory.getDefaultBus().setExtension(new URLConnectionHTTPConduitFactory(), HTTPConduitFactory.class);
+    }
 }
