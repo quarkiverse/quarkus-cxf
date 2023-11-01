daebdc2484e49560cc7b57c662002b0480e18faf
Peter Palaga <ppalaga@redhat.com>
2023-11-01 21:13:22 +0100

    Test schema.validation.enabled=true set via @SchemaValidation

---
+++ integration-tests/server/src/test/java/io/quarkiverse/cxf/it/server/XmlSchemaValidationIT.java
@@ -0,0 +1,8 @@
+package io.quarkiverse.cxf.it.server;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class XmlSchemaValidationIT extends XmlSchemaValidationTest {
+
+}
---
+++ integration-tests/server/src/test/java/io/quarkiverse/cxf/it/server/XmlSchemaValidationTest.java
@@ -0,0 +1,68 @@
+package io.quarkiverse.cxf.it.server;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.quarkiverse.cxf.it.server.xml.schema.validation.model.CalculatorService;
+import io.quarkiverse.cxf.test.QuarkusCxfClientTestUtil;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class XmlSchemaValidationTest {
+
+    @Test
+    public void addCheckParametersValid() {
+        /*
+         * Both operands fulfill the <= 1024 constraint of the smallIntType type defined in calculator.xsd
+         * and the return value is a valid int
+         */
+        Assertions.assertThat(getClient().addCheckParameters(1024, 1024)).isEqualTo(2048);
+    }
+
+    @Test
+    public void addCheckParametersInvalid() {
+        /*
+         * The first operand is not a valid value for the smallIntType type defined in calculator.xsd
+         * thus we expect and error
+         */
+        Assertions.assertThatThrownBy(() -> getClient().addCheckParameters(1028, 2))
+                .hasRootCauseInstanceOf(org.apache.cxf.binding.soap.SoapFault.class)
+                .rootCause()
+                .hasMessageContaining(
+                        "Unmarshalling Error: cvc-maxInclusive-valid: Value '1028' is not facet-valid with respect to maxInclusive '1024' for type 'smallIntType'.");
+
+        ;
+    }
+
+    @Test
+    public void addCheckResultValid() {
+        /*
+         * Both operands are valid integers
+         * and the return value still fulfills the <= 1024 constraint of the smallIntType type defined in calculator.xsd
+         */
+        Assertions.assertThat(getClient().addCheckResult(1028, -4)).isEqualTo(1024);
+    }
+
+    @Test
+    public void addCheckResultInvalid() {
+        /*
+         * Both operands are valid integers
+         * but the return value 1025 is not a valid value for the smallIntType (<= 1024) type defined in calculator.xsd
+         * thus we expect and error
+         */
+        Assertions.assertThatThrownBy(() -> getClient().addCheckResult(1024, 1))
+                .hasRootCauseInstanceOf(org.apache.cxf.binding.soap.SoapFault.class)
+                .rootCause()
+                .hasMessageContaining(
+                        "Marshalling Error: cvc-maxInclusive-valid: Value '1025' is not facet-valid with respect to maxInclusive '1024' for type 'smallIntType'.");
+    }
+
+    private CalculatorService getClient() {
+        final CalculatorService client = QuarkusCxfClientTestUtil.getClient(
+                "http://www.jboss.org/eap/quickstarts/wscalculator/Calculator",
+                CalculatorService.class,
+                "/soap/schema-validated-calculator");
+        return client;
+    }
+
+}
