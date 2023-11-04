efa55e6d58c27a8692903f7ea2e893eafa70848a
Peter Palaga <ppalaga@redhat.com>
2023-11-04 18:13:16 +0100

    Remove @Policy annotation from implementation classes as long as the policy is set on the interface

--- integration-tests/ws-security-policy/src/main/java/io/quarkiverse/cxf/it/security/policy/HttpsPolicyHelloServiceImpl.java
+++ integration-tests/ws-security-policy/src/main/java/io/quarkiverse/cxf/it/security/policy/HttpsPolicyHelloServiceImpl.java
@@ -3,13 +3,10 @@ package io.quarkiverse.cxf.it.security.policy;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 
-import org.apache.cxf.annotations.Policy;
-
 /**
  * A service implementation with a transport policy set
  */
 @WebService
-@Policy(placement = Policy.Placement.BINDING, uri = "https-policy.xml")
 public class HttpsPolicyHelloServiceImpl implements HttpsPolicyHelloService {
 
     @WebMethod
--- integration-tests/ws-security-policy/src/main/java/io/quarkiverse/cxf/it/security/policy/UsernameTokenPolicyHelloServiceImpl.java
+++ integration-tests/ws-security-policy/src/main/java/io/quarkiverse/cxf/it/security/policy/UsernameTokenPolicyHelloServiceImpl.java
@@ -3,13 +3,10 @@ package io.quarkiverse.cxf.it.security.policy;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 
-import org.apache.cxf.annotations.Policy;
-
 /**
  * A service implementation with a transport policy set
  */
 @WebService
-@Policy(placement = Policy.Placement.BINDING, uri = "username-token-policy.xml")
 public class UsernameTokenPolicyHelloServiceImpl implements UsernameTokenPolicyHelloService {
 
     @WebMethod
