c0458a9bdb59feedfb68a6157461a0df74c99599
Peter Palaga <ppalaga@redhat.com>
2023-11-23 14:20:45 +0100

    Make client integration tests runnable on Quarkus Platform

--- integration-tests/client/src/test/java/io/quarkiverse/cxf/it/logging/CxfLoggingTest.java
+++ integration-tests/client/src/test/java/io/quarkiverse/cxf/it/logging/CxfLoggingTest.java
@@ -3,7 +3,6 @@ package io.quarkiverse.cxf.it.logging;
 import static org.hamcrest.Matchers.is;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,12 +15,13 @@ import org.awaitility.Awaitility;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.jupiter.api.Test;
 
+import io.quarkiverse.cxf.client.it.CxfClientTestResource;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 
 @QuarkusTest
-@QuarkusTestResource(CxfLoggingTestResource.class)
+@QuarkusTestResource(CxfClientTestResource.class)
 public class CxfLoggingTest {
     private static final Pattern INSTALLED_FEATURES_PATTERN = Pattern.compile("Installed features: \\[[^\\]]*cxf[^\\]]*\\]");
 
@@ -113,49 +113,6 @@ public class CxfLoggingTest {
                             }
                             return false;
                         });
-    }
-
-    /**
-     * Make sure that our static copy is the same as the WSDL served by the container
-     *
-     * @throws IOException
-     */
-    @Test
-    void wsdlUpToDate() throws IOException {
-        final String wsdlUrl = ConfigProvider.getConfig()
-                .getValue("quarkus.cxf.client.beanConfiguredCalculator.wsdl", String.class);
-
-        Path staticCopyPath = Paths.get("src/main/resources/wsdl/CalculatorService.wsdl");
-        if (!Files.isRegularFile(staticCopyPath)) {
-            /*
-             * This test can be run from the test jar on Quarkus Platform
-             * In that case target/classes does not exist an we have to copy
-             * what's needed manually
-             */
-            staticCopyPath = Paths.get("target/classes/wsdl/CalculatorService.wsdl");
-            Files.createDirectories(staticCopyPath.getParent());
-            try (InputStream in = CxfLoggingTest.class.getClassLoader().getResourceAsStream("wsdl/CalculatorService.wsdl")) {
-                Files.copy(in, staticCopyPath);
-            }
-        }
-        /* The changing Docker IP address in the WSDL should not matter */
-        final String sanitizerRegex = "<soap:address location=\"http://[^/]*/calculator-ws/CalculatorService\"></soap:address>";
-        final String staticCopyContent = Files
-                .readString(staticCopyPath, StandardCharsets.UTF_8)
-                .replaceAll(sanitizerRegex, "");
-
-        final String expected = RestAssured.given()
-                .get(wsdlUrl)
-                .then()
-                .statusCode(200)
-                .extract().body().asString();
-
-        if (!expected.replaceAll(sanitizerRegex, "").equals(staticCopyContent)) {
-            Files.writeString(staticCopyPath, expected, StandardCharsets.UTF_8);
-            Assertions.fail("The static WSDL copy in " + staticCopyPath
-                    + " went out of sync with the WSDL served by the container. The content was updated by the test, you just need to review and commit the changes.");
-        }
-
     }
 
 }
