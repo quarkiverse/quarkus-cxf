632e89b04d548e98a7dd083c6a8e1d616ef46bf9
Peter Palaga <ppalaga@redhat.com>
2024-02-11 18:49:25 +0100

    Merge the WS-Security client and server modules into one to save some CI resources

--- integration-tests/ws-security-server/src/test/java/io/quarkiverse/cxf/it/ws/securitypolicy/server/CxfWssSecurityPolicyServerIT.java
+++
@@ -1,8 +0,0 @@
-package io.quarkiverse.cxf.it.ws.securitypolicy.server;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class CxfWssSecurityPolicyServerIT extends CxfWssSecurityPolicyServerTest {
-
-}
