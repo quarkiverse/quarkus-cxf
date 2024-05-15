fedd8f083c4f937867918c6447d8cd8b5b4a0cc4
Peter Palaga <ppalaga@redhat.com>
2024-05-15 12:01:51 +0200

    Test integration with @io.opentelemetry.instrumentation.annotations.WithSpan fix #1374

--- integration-tests/opentelemetry/src/test/java/io/quarkiverse/cxf/opentelemetry/it/OpenTelemetryTest.java
+++ integration-tests/opentelemetry/src/test/java/io/quarkiverse/cxf/opentelemetry/it/OpenTelemetryTest.java
@@ -95,6 +95,83 @@ public class OpenTelemetryTest {
 
     }
 
+    @Test
+    void traced() {
+
+        given()
+                .body("Joe")
+                .post("/opentelemetry/client/helloTraced")
+                .then()
+                .statusCode(200)
+                .body(CoreMatchers.equalTo("Hello traced Joe"));
+
+        Awaitility.await().atMost(5, TimeUnit.SECONDS).until(() -> {
+            List<Map<String, Object>> spans = getSpans();
+            System.out.println("=== spans " + spans.size() + " " + spans);
+            return spans.size() == 5;
+        });
+
+        final List<Map<String, Object>> spans = getSpans();
+
+        /* The ordering of the spans is not fully deterministic, so let's just check that all 4 are there */
+        final Map<String, Map<String, Object>> spansByName = new LinkedHashMap<>();
+        spans.forEach(span -> spansByName.put(span.get("name").toString(), span));
+        Assertions.assertThat(spansByName.size()).isEqualTo(5);
+
+        {
+            /* Quarkus CXF service span */
+            final Map<String, Object> span = spansByName.get("POST /soap/hello");
+            Assertions.assertThat(span.get("kind")).isEqualTo(SpanKind.SERVER.toString());
+            Assertions.assertThat(span.get("name")).isEqualTo("POST /soap/hello");
+
+            final Map<?, ?> attribs = (Map<?, ?>) span.get("attributes");
+            Assertions.assertThat(attribs.get("http.response.status_code")).isEqualTo(200);
+            Assertions.assertThat(attribs.get("http.request.method")).isEqualTo("POST");
+
+            /* TracedBean.helloTraced */
+            final Map<String, Object> tracedSpan = spansByName.get("TracedBean.helloTracedSpan");
+            Assertions.assertThat(tracedSpan.get("kind")).isEqualTo(SpanKind.INTERNAL.toString());
+            Assertions.assertThat(tracedSpan.get("name")).isEqualTo("TracedBean.helloTracedSpan");
+
+            /* TracedBean.helloTraced has the service as its parent */
+            Assertions.assertThat(tracedSpan.get("parentSpanId")).isEqualTo(span.get("spanId"));
+        }
+
+        {
+            /* quarkus-vertx-web span serving the Quarkus CXF service */
+            final Map<String, Object> span = spansByName.get("POST /soap/");
+            Assertions.assertThat(span.get("kind")).isEqualTo(SpanKind.SERVER.toString());
+            Assertions.assertThat(span.get("name")).isEqualTo("POST /soap/");
+
+            final Map<?, ?> attribs = (Map<?, ?>) span.get("attributes");
+            Assertions.assertThat(attribs.get("http.status_code")).isEqualTo(200);
+            Assertions.assertThat(attribs.get("http.method")).isEqualTo("POST");
+        }
+
+        {
+            /* Quarkus CXF client span */
+            final Map<String, Object> span = spansByName.get("POST http://localhost:8081/soap/hello");
+            Assertions.assertThat(span.get("kind")).isEqualTo(SpanKind.CLIENT.toString());
+            Assertions.assertThat(span.get("name")).isEqualTo("POST http://localhost:8081/soap/hello");
+
+            final Map<?, ?> attribs = (Map<?, ?>) span.get("attributes");
+            Assertions.assertThat(attribs.get("http.response.status_code")).isEqualTo(200);
+            Assertions.assertThat(attribs.get("http.request.method")).isEqualTo("POST");
+        }
+
+        {
+            /* quarkus-vertx-web span invoking the CXF client */
+            final Map<String, Object> span = spansByName.get("POST /opentelemetry/client/helloTraced");
+            Assertions.assertThat(span.get("kind")).isEqualTo(SpanKind.SERVER.toString());
+            Assertions.assertThat(span.get("name")).isEqualTo("POST /opentelemetry/client/helloTraced");
+
+            final Map<?, ?> attribs = (Map<?, ?>) span.get("attributes");
+            Assertions.assertThat(attribs.get("http.status_code")).isEqualTo(200);
+            Assertions.assertThat(attribs.get("http.method")).isEqualTo("POST");
+        }
+
+    }
+
     private static List<Map<String, Object>> getSpans() {
         return RestAssured.get("/opentelemetry/export").body().as(new TypeRef<>() {
         });
