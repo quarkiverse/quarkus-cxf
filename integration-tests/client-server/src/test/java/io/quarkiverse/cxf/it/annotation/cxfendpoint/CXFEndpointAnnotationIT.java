81868cade628a29c7ef45de95202d94d48182cfa
Peter Palaga <ppalaga@redhat.com>
2024-06-03 11:23:25 +0200

    Revert the @CXFEndpoint("/myPath") feature in 3.8 to avoid #1399
    This reverts commit d800b135bd77a475fcb053b163cb6c4a30cb8ae5.

--- integration-tests/client-server/src/test/java/io/quarkiverse/cxf/it/annotation/cxfendpoint/CXFEndpointAnnotationIT.java
+++
@@ -1,8 +0,0 @@
-package io.quarkiverse.cxf.it.annotation.cxfendpoint;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class CXFEndpointAnnotationIT extends CXFEndpointAnnotationTest {
-
-}
