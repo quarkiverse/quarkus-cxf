cf28c8c9dee6ebf178d3959610be35893000c000
Peter Palaga <ppalaga@redhat.com>
2024-11-14 11:40:55 +0100

    NullPointerException: HttpClientPool in org.apache.cxf.Bus using VertxHttpClientHTTPConduitFactory, fix #1579

--- extensions/core/deployment/src/main/java/io/quarkiverse/cxf/quarkus/vertx/http/client/deployment/VertxWebClientProcessor.java
+++ extensions/core/deployment/src/main/java/io/quarkiverse/cxf/quarkus/vertx/http/client/deployment/VertxWebClientProcessor.java
@@ -1,27 +1,15 @@
 package io.quarkiverse.cxf.quarkus.vertx.http.client.deployment;
 
-import io.quarkiverse.cxf.deployment.RuntimeBusCustomizerBuildItem;
-import io.quarkiverse.cxf.quarkus.vertx.http.client.VertxWebClientRecorder;
 import io.quarkiverse.cxf.vertx.http.client.HttpClientPool;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.annotations.ExecutionTime;
-import io.quarkus.deployment.annotations.Record;
 
 public class VertxWebClientProcessor {
 
     @BuildStep
     void additionalBeans(BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
         additionalBeans.produce(AdditionalBeanBuildItem.unremovableOf(HttpClientPool.class));
-    }
-
-    @BuildStep
-    @Record(ExecutionTime.STATIC_INIT)
-    void customizers(
-            VertxWebClientRecorder recorder,
-            BuildProducer<RuntimeBusCustomizerBuildItem> customizers) {
-        customizers.produce(new RuntimeBusCustomizerBuildItem(recorder.customizeBus()));
     }
 
 }
