ee9b211b5eefbb8490cfee014f62227992fbd819
Ladislav Thon <lthon@redhat.com>
2025-03-25 12:34:20 +0100

    Dev UI: always produce JsonRPCProvidersBuildItem
    This is due to an upcoming change in Execution Model Validation [1]
    where we need the `JsonRPCProvidersBuildItem` produced always, not
    only in dev mode. JSON RPC providers can use execution model affecting
    annotations, so we need to know about them, otherwise a non-dev build
    would fail with an incorrect validation error.
    
    [1] https://github.com/quarkusio/quarkus/pull/46965

--- extensions/core/deployment/src/main/java/io/quarkiverse/cxf/deployment/devui/DevUIProcessor.java
+++ extensions/core/deployment/src/main/java/io/quarkiverse/cxf/deployment/devui/DevUIProcessor.java
@@ -48,7 +48,7 @@ public class DevUIProcessor {
         return cardPageBuildItem;
     }
 
-    @BuildStep(onlyIf = IsDevelopment.class)
+    @BuildStep
     JsonRPCProvidersBuildItem createJsonRPCServiceForCache() {
         return new JsonRPCProvidersBuildItem(CxfJsonRPCService.class);
     }
