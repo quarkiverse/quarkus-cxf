3ca1061d4c82e21407f5509db8c987e76bd2afa8
Peter Palaga <ppalaga@redhat.com>
2025-05-13 10:39:32 +0200

    Avoid Getting 429 Too Many Requests when GET-ting https://github.com/<org>/<repo>/(?:blob|tree)/.* by using https://api.github.com/repos/OWNER/REPO/contents/PATH instead, fix #1804

--- docs/src/test/java/io/quarkiverse/cxf/doc/it/AntoraTest.java
+++ docs/src/test/java/io/quarkiverse/cxf/doc/it/AntoraTest.java
@@ -8,10 +8,14 @@ import java.util.Set;
 import java.util.concurrent.TimeoutException;
 import java.util.regex.Pattern;
 
+import org.assertj.core.api.Assertions;
 import org.hamcrest.CoreMatchers;
+import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Test;
 
 import io.quarkiverse.antorassured.AntorAssured;
+import io.quarkiverse.antorassured.LinkGroupFactory;
+import io.quarkiverse.antorassured.LinkStream;
 import io.quarkiverse.antorassured.ResourceResolver;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
@@ -19,6 +23,8 @@ import io.restassured.http.ContentType;
 
 @QuarkusTest
 public class AntoraTest {
+
+    private static final Logger log = Logger.getLogger(AntoraTest.class);
 
     @Test
     public void antoraSite() throws TimeoutException, IOException, InterruptedException {
@@ -41,10 +47,31 @@ public class AntoraTest {
             //ignorables.add("https://quarkus.io/blog/quarkus-3-19-1-released/");
         }
 
-        AntorAssured
+        LinkStream linkStream = AntorAssured
                 .links()
                 .excludeResolved(Pattern.compile("^\\Qhttp://localhost:808\\E[02].*"))
                 .excludeEditThisPage()
+                .overallTimeout(120_000L);
+
+        final String ghToken = System.getenv("GITHUB_TOKEN");
+        if (ghToken == null) {
+            if (Boolean.parseBoolean(System.getenv("GITHUB_ACTIONS"))) {
+                Assertions.fail(
+                        "Set GITHUB_TOKEN environment variable to test GitHub links - see https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28");
+            } else {
+                log.warn(
+                        "Set GITHUB_TOKEN environment variable to test GitHub links - see https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28");
+                linkStream = linkStream
+                        .excludeResolved(Pattern.compile("https://github.com/[^/]+/[^/]+/(:?blob|tree)/.*"));
+            }
+        } else {
+            log.info("GITHUB_TOKEN set");
+            linkStream = linkStream
+                    .group(LinkGroupFactory.gitHubRawBlobLinks(ghToken))
+                    .endGroup();
+        }
+
+        linkStream
                 .validate()
                 .ignore(err -> ignorables.contains(err.uri().resolvedUri()))
                 .assertValid();
