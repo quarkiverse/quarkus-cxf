d6e3052a9da6d81ddd742e225feb5b9e5e3f11b5
Peter Palaga <ppalaga@redhat.com>
2026-08-25 12:35:36 +0200

    Fixup #2240 Transform ReceiveTimeoutLaggyServerTest from QuarkusExtensionTest into a @QuarkusTest/@QuarkusIntegrationTest

---
+++ integration-tests/metrics/src/test/java/io/quarkiverse/cxf/metrics/it/ReceiveTimeoutLaggyServerIT.java
@@ -0,0 +1,7 @@
+package io.quarkiverse.cxf.metrics.it;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ReceiveTimeoutLaggyServerIT extends ReceiveTimeoutLaggyServerTest {
+}
